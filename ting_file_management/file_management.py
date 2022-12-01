13a9fa3ce429e5a13d5537f9f8f98f711b19686a
fabionicolau <fabionicolaujr@gmail.com>
2022-12-01 14:57:16 -0300

    feat: implementa função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            text = file.readlines()
+            formated_text = [line.strip() for line in text]
+            return formated_text
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
+print(txt_importer("statics/arquivo_teste.txt"))
